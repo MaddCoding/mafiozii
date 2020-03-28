6fd3ff5ed9be5bdaca0f31b65dccc3d616a4b667
MaddCoding <MaddCoding@users.noreply.github.com>
2020-03-28 22:21:24 +0300

    maf

---
+++ config.py
@@ -0,0 +1,14 @@
+TOKEN = '1135503249:AAEc2kxRj5fmJPzH418L-rLiZCLEelTRkm0'  # Learn how to get one on https://core.telegram.org/bots#6-botfather
+ADMIN_ID = 1104692268
+PLAYERS_COUNT_TO_START = 4  # Minimal quantity of players that is required to start mafia game
+PLAYERS_COUNT_LIMIT = 10  # Maximum quantity of players that is allowed to start mafia game
+REQUEST_OVERDUE_TIME = 10 * 60  # Time (in seconds) of request's inactivity after which it gets deleted from database
+SERVER_IP = '104.154.193.240 '
+SERVER_PORT = 267507
+WORD_BASE = 'C:/Users/Dante/source/repos/PythonApplication1/PythonApplication1/words.txt'  # Used by crocodile game
+#SSL_CERT = '/root/cert/PUBLIC.pem'  # File path of public SSL certificate 
+#SSL_PRIV = '/root/cert/PRIVATE.key'  # File path of private key of SSL certificate
+
+import logging
+LOGGER_LEVEL = logging.DEBUG
+
